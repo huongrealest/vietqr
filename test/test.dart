afa6e868cae44469958ecc759897b94c908774cc
Ken <>
2025-01-21 15:05:55 +0700

    test: add initial test for VietQR QR code generation

---
+++ test/test.dart
@@ -0,0 +1,12 @@
+import 'package:vietqr/vietqr.dart';
+
+void main() {
+  final qr = QrPay.vietQR(
+    bin: '970407',
+    bankNumber: '0386577672',
+    purpose: 'henry an cut',
+    amount: '1000000',
+  ).build();
+  print(qr);
+  // 00020101021138540010A00000072701240006970407011003865776720208QRIBFTTA5303704540710000005802VN62160812henry an cut6304A322
+}
